4544bcefdc0b2661b9ef8ef43df235215d0ad55c
cmyui <cmyuiosu@gmail.com>
2018-11-10 02:48:53 -0500

    Messaging channel

--- discordcmd.py
+++ discordcmd.py
@@ -13,9 +13,8 @@ config.read('config.ini')
 @client.event
 async def on_ready():
     print('Logged in as:\n{}\n{}\n------'.format(client.user.name,client.user.id))
-    print("\n\n\n\n\nextras: {}\ndebug: {}\ntokenauth: {}\n\n\n\n".format(config['default']['extras'], config['default']['debug'], config['default']['tokenauth']))
     if int(config['default']['debug']) == 1:
-        print("\nDebug 1 ---------------------------------------------------------------------- #\n\n")
+        print("\nDebug 1 ---------------------------------------------------------------------- #\n\nConfiguration:\nextras: {}\ndebug: {}\ntokenauth: {}\n\n".format(config['default']['extras'], config['default']['debug'], config['default']['tokenauth']))
 
     # Our little list of possibilities..
     if int(config['default']['extras']) == 1:
@@ -41,16 +40,12 @@ async def on_ready():
         if int(config['default']['debug']) == 1:
             print("\nDebug 3 ---------------------------------------------------------------------- #\ntargetServerID: {}\n\n".format(targetServerID))
         for server in client.servers:
-            """
             if int(config['default']['debug']) == 1:
                 print("\nDebug 4 ---------------------------------------------------------------------- #\n\n")
                 print("Current Server: {}\n".format(server))
-            """
             if server.id == str(targetServerID):
-                """
                 if int(config['default']['debug']) == 1:
                     print("\nDebug 5 ---------------------------------------------------------------------- #\n\n")
-                """
                 targetServer = server
                 print('Server found.\n')
 
@@ -125,71 +120,39 @@ async def on_ready():
                 targetUser.server_permissions.manage_webhooks,
                 targetUser.server_permissions.manage_emojis
                 ))
-
-            """ Old ver
-            print("------ User Perms ------")
-            print("Permissions Bitwise: {}\n".format(targetUser.server_permissions.value))
-            print("create_instant_invite: {}".format(targetUser.server_permissions.kick_members))
-            print("kick members: {}".format(targetUser.server_permissions.create_instant_invite))
-            print("ban_members: {}".format(targetUser.server_permissions.ban_members))
-            print("administrator: {}".format(targetUser.server_permissions.administrator))
-            print("manage_channels: {}".format(targetUser.server_permissions.manage_channels))
-            print("manage_server: {}".format(targetUser.server_permissions.manage_server))
-            print("view_audit_logs: {}".format(targetUser.server_permissions.view_audit_logs))
-            print("read_messages: {}".format(targetUser.server_permissions.read_messages))
-            print("send_messages: {}".format(targetUser.server_permissions.send_messages))
-            print("send_tts_messages: {}".format(targetUser.server_permissions.send_tts_messages))
-            print("manage_messages: {}".format(targetUser.server_permissions.manage_messages))
-            print("embed_links: {}".format(targetUser.server_permissions.embed_links))
-            print("attach_files: {}".format(targetUser.server_permissions.attach_files))
-            print("read_message_history: {}".format(targetUser.server_permissions.read_message_history))
-            print("mention_everyone: {}".format(targetUser.server_permissions.mention_everyone))
-            print("external_emojis: {}".format(targetUser.server_permissions.external_emojis))
-            print("connect: {}".format(targetUser.server_permissions.connect))
-            print("speak: {}".format(targetUser.server_permissions.speak))
-            print("mute_members: {}".format(targetUser.server_permissions.mute_members))
-            print("deafen_members: {}".format(targetUser.server_permissions.deafen_members))
-            print("move_members: {}".format(targetUser.server_permissions.move_members))
-            print("use_voice_activation: {}".format(targetUser.server_permissions.use_voice_activation))
-            print("change_nickname: {}".format(targetUser.server_permissions.change_nickname))
-            print("manage_nicknames: {}".format(targetUser.server_permissions.manage_nicknames))
-            print("manage_roles: {}".format(targetUser.server_permissions.manage_roles))
-            print("manage_webhooks: {}".format(targetUser.server_permissions.manage_webhooks))
-            print("manage_emojis: {}".format(targetUser.server_permissions.manage_emojis))
-            """
         else:
 
             if int(config['default']['debug']) == 1:
                 print("\nDebug 8 ---------------------------------------------------------------------- #\n\n")
                 print("Sorry.. I couldn't find a user by that ID.. Heres a bit of info:\n")
-                print("------ Debug ------")
                 print("Server:")
                 print("targetServerID: {}".format(targetServerID))
                 print("targetServer: {}\n".format(targetServer))
                 print("User:")
                 print("targetUserID: {}".format(targetUserID))
-                print("targetUser: {}".format(targetUser))
-                print("------ Debug ------\n\n")
+                print("targetUser: {}\n\n".format(targetUser))
             else:
                 return "Sorry.. I couldn't find a user by that ID!"
 
     # Really stupid, and incomplete stuff
     elif actionDesiredInt == 2 and int(config['default']['extras']) == 1:
         # 2. Post a message as the user (in a server)
-        targetServerID = input('\nAlright. I\'ll need the server ID of the discord server you\'d like to post in.\n>> ')
-        for server in client.servers:
-            if server.id == str(targetServerID):
-                targetServer = server
-                print('Server found.\n')
-            else:
-                return 'Sorry.. I couldn\'t find a server by that ID!'
+        targetChannelID = input('\nAlright. I\'ll need the channel ID of where you\'d like to post in.\n>> ')
+        targetChannel = client.get_channel(targetChannelID)
+        if targetChannel is not None:
+            print("Channel found; Connection established.")
+
+            x = 0 # Sorry I literally do not know how to do this properly. Maybe this is properly.. but I doubt it
+            while x < 1:
+                message = input("\n{}: ".format(config['discord']['email']))
+                await client.send_message(targetChannel, message)
 
     elif actionDesiredInt == 3 and int(config['default']['extras']) == 1:
         # 3. Open PM and send a message to a user
         targetUserID = input('Whats their UserID?\n>> ')
         if targetUserID is not None:
             client.start_private_message(targetUserID)
-            await client.send_message(targetUserID, 'Test')
+            await client.send_message(message.channel, 'Test')
         else:
             return 'Could not find a user by that ID.'
 
@@ -211,10 +174,16 @@ if int(config['default']['debug']) == 1:
     print("Beginning login process..\n")
 
 if int(config['default']['tokenauth']) == 1:
+    """
     if int(config['default']['debug']) == 1:
         print("Input: {}".format(config['discord']['token']))
+    """
     client.run(str(config['discord']['token']))
-else:
+elif int(config['default']['tokenauth']) == 0:
+    """
     if int(config['default']['debug']) == 1:
         print('Input: {}, {}'.format(config['discord']['email'], config['discord']['password']))
+    """
     client.run('{}'.format(config['discord']['email']), '{}'.format(config['discord']['password']))
+else:
+    print('ok ur too retarded for this program\n\nplease step outside')
