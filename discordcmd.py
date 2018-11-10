172565a248b5c5c89d8c9c90ab5b23e743981739
cmyui <cmyuiosu@gmail.com>
2018-11-10 00:40:05 -0500

    Config, and some breakdown of the stuff to add

--- discordcmd.py
+++ discordcmd.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import configparser
 
 client = discord.Client()
 
@@ -10,4 +11,36 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.run('token')
+    # Our little list of possibilities..
+    actionDesired = input("""\nWhat would you like to do today?
+                            \n1. Check a users permissions
+                            \n2. Post a message as the user
+                            \n3. DOX User (Currently Disabled)
+                            \n4. Check bans in a specific server
+                            \n5. Check all bans the user can see
+                            \n6. cmyuiDiscord (Beta as fuck)
+                            \n>> """)
+
+    if actionDesired.find("1"):
+        # 1. Check a users permissions
+        return False
+    elif actionDesired.find("2"):
+        # 2. Post a message as the user
+        return False
+    elif actionDesired.find("3"):
+        # 3. DOX User (Currently Disabled)
+        return False
+    elif actionDesired.find("4"):
+        # 4. Check bans in a specific server
+        return False
+    elif actionDesired.find("5"):
+        # 5. Check all bans the user can see
+        return False
+    elif actionDesired.find("6"):
+        # 6. cmyuiDiscord (Beta as fuck)
+        return False
+
+if config['default']['tokenAuth'] == True:
+    client.run(str(config['discord']['token']))
+else:
+    client.run('{}, {}'.format(config['discord']['email'], config['discord']['password'])
